18220c4989e62da356d7849caff91a987473e09f
Amandabuch <amandalbuch@gmail.com>
2020-05-04 13:48:47 -0700

    Basic model

--- main_app/models.py
+++ main_app/models.py
@@ -1,4 +1,27 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 
+MOODS = (
+    ('A', 'Angry'),
+    ('B', 'Melancholy'),
+    ('C', 'Cheerful'),
+    ('D', 'Calm'),
+    ('E', 'Romantic'),
+    ('F', 'Mysterious')
+)
+
+class Song(models.Model):
+    mood = models.CharField(
+        max_length=1,
+        choices=MOODS,
+        default=MOODS[0][0]
+    )
+    name = models.CharField(max_length=100)
+    band = models.CharField(max_length=100)
+    
+
+
+	# def __str__(self):
+    #     return f"{self.name} on {self.band}"
