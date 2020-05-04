0aba7983146e6a11ec3865b54a16e4be86b7c78c
John Khrizhtian Trujillo <yiannix@johns-mbp.home>
2020-05-04 11:49:51 -0700

    Implement Auth

--- main_app/views.py
+++ main_app/views.py
@@ -1,8 +1,15 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
 # Define the home view
 def home(request):
   return render(request, 'home.html')
 
+@login_required
 def about(request):
-    return render(request, 'about.html')
+  return render(request, 'about.html')
+
+def accounts(request):
+  return render(request, 'registration/login.html')
+
+    
